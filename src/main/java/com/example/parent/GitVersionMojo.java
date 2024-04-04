241d870f784546f111fd0deafd3597d315f477b9
benjamin.slabbert <benjamin.slabbert@gmail.com>
2024-04-04 21:07:15 +0200

    update build

--- src/main/java/com/example/parent/GitVersionMojo.java
+++ src/main/java/com/example/parent/GitVersionMojo.java
@@ -52,26 +52,19 @@ public class GitVersionMojo extends AbstractMojo {
       }
 
       Ref lastTag = repo.getRefDatabase().getRefsByPrefix(R_TAGS).getLast();
-      String version = lastTag.getName().substring(R_TAGS.length());
+      String version;
+      if (useSnapshotVersion) {
+        version = branch + "-SNAPSHOT";
+      } else {
+        version = lastTag.getName().substring(R_TAGS.length());
+      }
+
       project.getProperties().put(versionProperty, version);
       getLog()
           .info(
               String.format(
                   "updated property: %s=%s",
                   versionProperty, project.getProperties().get(versionProperty)));
-
-      //      // Create a new tag
-      //      String tagName = "v1.0.0-" + System.currentTimeMillis();
-      //      try (Git git = new Git(repo)) {
-      //        git.tag().setName(tagName).call();
-      //        // might need this for the remote?
-      ////        git.push().setPushTags().call();
-      //      } catch (GitAPIException e) {
-      //        throw new MojoExecutionException("Failed to create git tag", e);
-      //      }
-      //
-      //      getLog().info("Created git tag: " + tagName);
-
     } catch (IOException e) {
       throw new MojoExecutionException(e);
     }
